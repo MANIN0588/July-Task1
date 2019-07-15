0f2d4f25bb6d38bb68aebf4ac2efcf3d23453ede
MANIN0588 <manijareddy123@gmail.com>
2019-07-15 00:32:51 -0500

    changes to the application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { LogComponent } from './log/log.component';
+import{ RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+const appRoutes: Routes = [
+  {
+    path: 'log',
+    component: LogComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  }
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LogComponent,
+    LoginComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
     BrowserModule,
     AppRoutingModule
   ],
